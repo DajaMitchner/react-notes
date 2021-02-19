e7483800d9b220b0c1012ca2cbdc0f76ff8c7d27
Daja Mitchner <daja.mitchner@yahoo.com>
2021-02-18 21:50:00 -0500

    injecting student list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+
+
+let studentData = [
+  {
+    id: 1,
+    name: "Eddy",
+    color: "chartreuse",
+    school: "Nebula Academy",
+  },
+  {
+    id: 2,
+    name: "Chelsea",
+    color: "purple",
+    school: "Nebula Academy",
+  },
+  {
+    id: 3,
+    name: "Tauseef",
+    color: "orange",
+    school: "Nebula Academy",
+  },
+  {
+    id: 4, 
+    name: "Maurice", 
+    color: "red", 
+    school: "Nebula Academy",
+  },
+  { 
+    id: 5, 
+    name: "D'aja", 
+    color: "blue", 
+    school: "Nebula Academy", 
+  },
+  {
+     id: 6, 
+     name: "Jasmine", 
+     color: "pink", 
+     school: "Nebula Academy", 
+  },
+  { 
+    id: 7,
+     name: "Anitria",
+      color: "blue", 
+      school: "Nebula Academy",
+  },];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <StudentList />
+    <StudentList />
+    <StudentList />
+    <StudentList />
     </div>
   );
 }
 
+function StudentList() {
+  return studentData.map((student) => (
+    <div className='studentCard'>
+      <p>{student.name} is a {student.school} student</p>
+    </div>
+  ))
+}
 export default App;
