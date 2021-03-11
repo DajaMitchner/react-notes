9a59780f65efb97db8640a628245238f3d99d1f9
Daja Mitchner <daja.mitchner@yahoo.com>
2021-03-10 20:14:25 -0500

    react changes

--- src/App.js
+++ src/App.js
@@ -27,3 +27,79 @@ function App(){
 }
 export default App;
 
+const App = ( ) => {
+  const stories = [ ... ];
+
+return (
+  <div>
+    <h1>My Hacker Stories</h1>
+    <Search/>
+    <hr />
+
+    <List list={stories}/>
+  </div>
+);
+};
+const Search = ( ) => {
+  const [searchTerm, setSearchTerm] = React.useState('');
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+  }
+  return (
+    <div>
+      <label htmlFor='search'>Search: </label>
+      <input id='search' type='text' onChange={handleChange}/>
+
+      <p>
+        Searching for <strong>{searchTerm}</strong>
+      </p>
+    </div>
+  )
+}
+//A
+const handleSearch = event => {
+  console.log(event.target.value)
+
+return (
+  <div> 
+    <h1>My Hacker Stories</h1>
+    <Search onSearch={handleSearch}/>
+    <hr/>
+    <List list={stories}/>
+  </div>
+);
+}
+const Search = props => {
+  const [searchTerm,setSearchTerm] = React.useState('');
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+
+    //B
+    props.onSearch(event)
+  };
+  return (...);
+};
+const App = () => {
+  const stories = [ ... ];
+
+  const [searchTerm, setSearchTerm] = React.useState('');
+
+  const handleSearch = event => {
+    setSearchTerm(event.target.value)
+  }
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+      <Search onSearch ={handleSearch} />
+      <hr />
+
+      <List list={stories}/>
+    </div>
+  )
+}
+const Search =props => (
+  <div>
+    <label htmlFor='search'>Search: </label>
+    <input id='search' type='text' onChange={props.onSearch} />
+  </div>
+)
